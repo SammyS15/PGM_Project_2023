d4140324c4ccfd5b51a791b9e3e113301b83b85f
Sammy Sharief <sharief2@illinois.edu>
2023-12-07 23:42:49 -0500

    Updated Real_Data.csv and got Type II and Type Ibc

--- Notebooks/read_yse_ztf_snana_dir.py
+++ Notebooks/read_yse_ztf_snana_dir.py
@@ -43,7 +43,9 @@ def read_YSE_ZTF_snana_dir(dir_name, keep_ztf=True):
 
     print("Reading YSE+ZTF SNANA-style data files from directory: ", dir_name)
     
-    
+    ''' 
+    This assumes the project folder is in Downloads AND the folder called DATA_PGM_Project is also in Downloads
+    '''
     for file_path in sorted(glob.glob('../Photpipe/'+dir_name+'/*')):
         #print(file_path)
     
